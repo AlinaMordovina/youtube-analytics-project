3e18fa922aa6458359f7a77494cddaa2889fd373
Alina Mordovina <alina182@yandex.ru>
2023-11-22 14:28:44 +0400

    correct3 homework_2

--- src/channel.py
+++ src/channel.py
@@ -11,11 +11,11 @@ class Channel:
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
 
-        self.channel_id = channel_id
-        self.channel = self.get_service().channels().list(id=channel_id, part='snippet,statistics').execute()
+        self.__channel_id = channel_id
+        self.channel = self.get_service().channels().list(id=self.__channel_id, part='snippet,statistics').execute()
         self.title = self.channel['items'][0]['snippet']['title']
         self.description = self.channel['items'][0]['snippet']['description']
-        self.url = f"https://www.youtube/channels/{self.channel_id}"
+        self.url = f"https://www.youtube/channels/{self.__channel_id}"
         self.subscriber_count = int(self.channel['items'][0]['statistics']['subscriberCount'])
         self.video_count = int(self.channel['items'][0]['statistics']['videoCount'])
         self.view_count = int(self.channel['items'][0]['statistics']['viewCount'])
@@ -48,6 +48,16 @@ class Channel:
 
     def __ge__(self, other):
         return self.subscriber_count >= other.subscriber_count
+
+    @property
+    def channel_id(self):
+        return self.__channel_id
+
+    @channel_id.setter
+    def channel_id(self, new_channel_id):
+        if new_channel_id:
+            print("AttributeError: property 'channel_id' of 'Channel' object has no setter")
+            self.__channel_id = self.__channel_id
 
     @classmethod
     def get_service(cls):
