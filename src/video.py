b9d666f972d98af4386f17faace7bfbd7a946847
Alina Mordovina <alina182@yandex.ru>
2023-12-10 21:11:52 +0400

    homework_6

--- src/video.py
+++ src/video.py
@@ -7,13 +7,20 @@ class Video:
     youtube = build('youtube', 'v3', developerKey=api_key)
 
     def __init__(self, video_id):
-        self.video_id = video_id
-        self.video_response = self.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                                         id=self.video_id).execute()
-        self.video_title = self.video_response['items'][0]['snippet']['title']
-        self.url = f"https://youtu.be/{self.video_id}"
-        self.view_count = int(self.video_response['items'][0]['statistics']['viewCount'])
-        self.like_count = int(self.video_response['items'][0]['statistics']['likeCount'])
+        try:
+            self.video_id = video_id
+            self.video_response = self.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                             id=self.video_id).execute()
+            self.video_title = self.video_response['items'][0]['snippet']['title']
+            self.url = f"https://youtu.be/{self.video_id}"
+            self.view_count = int(self.video_response['items'][0]['statistics']['viewCount'])
+            self.like_count = int(self.video_response['items'][0]['statistics']['likeCount'])
+        except IndexError:
+            self.video_id = video_id
+            self.video_title = None
+            self.url = None
+            self.view_count = None
+            self.like_count = None
 
     def __str__(self):
         return self.video_title
